f2942164e98450a471ce8d299c12628516b0d964
KaweCh <102481713+KaweCh@users.noreply.github.com>
2025-04-17 14:23:05 +0700

    Add files via upload

--- XiaomiReFund.Domain/Entities/rms_CallbackQueue.cs
+++ XiaomiReFund.Domain/Entities/rms_CallbackQueue.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace XiaomiReFund.Domain.Entities
 {
@@ -14,163 +10,99 @@ namespace XiaomiReFund.Domain.Entities
         /// <summary>
         /// รหัสรายการในคิว
         /// </summary>
-        public int QueueID { get; private set; }
+        public int CallbackID { get; set; }
 
         /// <summary>
         /// รหัสการคืนเงิน
         /// </summary>
-        public int RefundID { get; private set; }
-
-        /// <summary>
-        /// รหัสเทอร์มินัล
-        /// </summary>
-        public string TerminalID { get; private set; }
-
-        /// <summary>
-        /// วันที่ทำรายการ
-        /// </summary>
-        public DateTime TransactionDate { get; private set; }
-
-        /// <summary>
-        /// รหัสธุรกรรม
-        /// </summary>
-        public string TransactionID { get; private set; }
-
-        /// <summary>
-        /// รหัสคำขอ
-        /// </summary>
-        public string RequestID { get; private set; }
-
-        /// <summary>
-        /// จำนวนเงินที่คืน
-        /// </summary>
-        public decimal RefundAmount { get; private set; }
+        public int RefundID { get; set; }
 
         /// <summary>
         /// สถานะ (Approved, Rejected)
         /// </summary>
-        public string Status { get; private set; }
+        public string Status { get; set; }
 
         /// <summary>
         /// ข้อความสถานะ
         /// </summary>
-        public string StatusMessage { get; private set; }
-
-        /// <summary>
-        /// ประเภทการชำระเงิน
-        /// </summary>
-        public string PaymentType { get; private set; }
+        public string StatusMessage { get; set; }
 
         /// <summary>
         /// จำนวนครั้งที่ลองส่งแล้ว
         /// </summary>
-        public int RetryCount { get; private set; }
+        public int RetryCount { get; set; }
 
         /// <summary>
         /// เวลาที่กำหนดให้ส่ง
         /// </summary>
-        public DateTime ScheduledTime { get; private set; }
+        public DateTime ScheduledTime { get; set; }
 
         /// <summary>
         /// สถานะการส่ง
         /// </summary>
-        public bool IsProcessed { get; private set; }
+        public bool IsProcessed { get; set; }
 
         /// <summary>
         /// เวลาที่ส่งสำเร็จ
         /// </summary>
-        public DateTime? ProcessedTime { get; private set; }
+        public DateTime? ProcessedTime { get; set; }
 
         /// <summary>
         /// วันที่สร้าง
         /// </summary>
-        public DateTime CreateDate { get; private set; }
+        public DateTime CreateDate { get; set; }
 
         /// <summary>
         /// วันที่อัปเดต
         /// </summary>
-        public DateTime? UpdateDate { get; private set; }
+        public DateTime? UpdateDate { get; set; }
 
-        /// <summary>
-        /// รหัสผู้ใช้ที่แก้ไข
-        /// </summary>
-        public int ModifiedBy { get; private set; }
-
-        // Navigation properties
-        /// <summary>
-        /// รายการคืนเงินที่เกี่ยวข้อง
-        /// </summary>
-        public virtual rms_OrderRefund Refund { get; private set; }
-
-        /// <summary>
-        /// ผู้ใช้ที่แก้ไข
-        /// </summary>
-        public virtual sys_Users ModifiedByUser { get; private set; }
-
-        private rms_CallbackQueue()
-        {
-            // Required by EF Core
-        }
+        // Navigation property
+        public virtual rms_OrderRefund Refund { get; set; }
 
         /// <summary>
         /// สร้างรายการคิวใหม่
         /// </summary>
         public static rms_CallbackQueue Create(
             int refundId,
-            string terminalId,
-            DateTime transactionDate,
-            string transactionId,
-            string requestId,
-            decimal refundAmount,
             string status,
             string statusMessage,
-            string paymentType,
             int retryCount,
-            DateTime scheduledTime,
-            int modifiedBy)
+            DateTime scheduledTime)
         {
             return new rms_CallbackQueue
             {
                 RefundID = refundId,
-                TerminalID = terminalId,
-                TransactionDate = transactionDate,
-                TransactionID = transactionId,
-                RequestID = requestId,
-                RefundAmount = refundAmount,
                 Status = status,
                 StatusMessage = statusMessage,
-                PaymentType = paymentType,
                 RetryCount = retryCount,
                 ScheduledTime = scheduledTime,
                 IsProcessed = false,
                 ProcessedTime = null,
-                CreateDate = DateTime.Now,
-                ModifiedBy = modifiedBy
+                CreateDate = DateTime.Now
             };
         }
 
         /// <summary>
         /// อัปเดตสถานะการประมวลผล
         /// </summary>
-        public void UpdateProcessingStatus(bool isProcessed, int modifiedBy)
+        public void UpdateProcessingStatus(bool isProcessed)
         {
             IsProcessed = isProcessed;
             if (isProcessed)
             {
                 ProcessedTime = DateTime.Now;
             }
-            ModifiedBy = modifiedBy;
             UpdateDate = DateTime.Now;
         }
 
         /// <summary>
         /// เพิ่มจำนวนครั้งที่ลองส่ง
         /// </summary>
-        public void IncrementRetryCount(DateTime nextScheduledTime, int modifiedBy)
+        public void IncrementRetryCount(DateTime nextScheduledTime)
         {
             RetryCount++;
             ScheduledTime = nextScheduledTime;
-            ModifiedBy = modifiedBy;
             UpdateDate = DateTime.Now;
         }
     }
--- XiaomiReFund.Infrastructure/Data/EntityConfigurations/rms_CallbackQueueConfiguration.cs
+++ XiaomiReFund.Infrastructure/Data/EntityConfigurations/rms_CallbackQueueConfiguration.cs
@@ -1,12 +1,75 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XiaomiReFund.Domain.Entities;
 
 namespace XiaomiReFund.Infrastructure.Data.EntityConfigurations
 {
-    internal class rms_CallbackQueueConfiguration
+    /// <summary>
+    /// การกำหนดค่าเอนทิตี Callback Queue
+    /// </summary>
+    public class rms_CallbackQueueConfiguration : IEntityTypeConfiguration<rms_CallbackQueue>
     {
+        public void Configure(EntityTypeBuilder<rms_CallbackQueue> builder)
+        {
+            // กำหนดชื่อตาราง
+            builder.ToTable("rms_CallbackQueue");
+
+            // กำหนด Primary Key
+            builder.HasKey(e => e.CallbackID);
+
+            // กำหนดคุณสมบัติแต่ละฟิลด์
+            builder.Property(e => e.CallbackID)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+            builder.Property(e => e.RefundID)
+                .IsRequired();
+
+            builder.Property(e => e.Status)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(e => e.StatusMessage)
+                .HasMaxLength(255);
+
+            builder.Property(e => e.RetryCount)
+                .HasDefaultValue(0)
+                .IsRequired();
+
+            builder.Property(e => e.ScheduledTime)
+                .HasColumnType("datetime")
+                .IsRequired();
+
+            builder.Property(e => e.IsProcessed)
+                .HasDefaultValue(false)
+                .IsRequired();
+
+            builder.Property(e => e.ProcessedTime)
+                .HasColumnType("datetime");
+
+            builder.Property(e => e.CreateDate)
+                .HasDefaultValueSql("getdate()")
+                .IsRequired();
+
+            builder.Property(e => e.UpdateDate)
+                .HasColumnType("datetime");
+
+            // กำหนด Indexes
+            builder.HasIndex(e => e.RefundID);
+            builder.HasIndex(e => e.IsProcessed);
+            builder.HasIndex(e => e.ScheduledTime);
+            builder.HasIndex(e => e.ProcessedTime);
+
+            // กำหนด Relationships
+            builder.HasOne(e => e.Refund)
+                .WithMany()
+                .HasForeignKey(e => e.RefundID)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
